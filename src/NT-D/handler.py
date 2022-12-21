0e29d8974d15e9b6d5f3abc892f4b1a8a209024e
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2022-12-22 01:46:24 +0800

    create handler.py for deploy model

---
+++ src/NT-D/handler.py
@@ -0,0 +1,32 @@
+import json
+from typing import Any
+
+from torch import Tensor
+from ts.torch_handler.base_handler import BaseHandler
+
+
+class ModelHandler(BaseHandler):
+	def __init__(self):
+		self.context: Any = None
+		self.initialized: bool = False
+		self.explain: bool = False
+		self.target: int = 0
+
+	def initialize(self, context) -> None:
+		self.context = context
+		self.initialized = True
+
+	def preprocess(self, data) -> Tensor:
+		input_data: dict = json.load(data)
+
+	def inference(self, model_input):
+		return self.model(model_input)
+
+	def postprocess(self, inference_output) -> str:
+		return '{:.2f}%'.format(inference_output * 100)
+
+	def handle(self, data, context) -> str:
+		model_input: Tensor = self.preprocess(data)
+		model_output = self.inference(model_input)
+
+		return self.postprocess(model_output)
