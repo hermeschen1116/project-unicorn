63702559f3b5bc4899696314efdf2e5ee822653d
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2022-12-23 05:01:17 +0800

    fix some bug but there still some

--- src/NT-D/Docker/handler.py
+++ src/NT-D/Docker/handler.py
@@ -1,9 +1,11 @@
 import json
+import os
 from math import sqrt
 from typing import Any
 
 import numpy
 import numpy as np
+import torch
 from numpy import array
 from sklearn import preprocessing
 from torch import Tensor
@@ -13,17 +15,32 @@ from ts.torch_handler.base_handler import BaseHandler
 class ModelHandler(BaseHandler):
 	def __init__(self):
 		super().__init__()
-		self.context: Any = None
+		self._context: Any = None
 		self.initialized: bool = False
-		self.explain: bool = False
-		self.target: int = 0
+		self.model: Any = None
+		self.device: Any = None
 
 	def initialize(self, context) -> None:
-		self.context = context
+		self.manifest = context.manifest
+		properties = context.system_properties
+		model_dir = properties.get("model_dir")
+		self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
+
+		# Read model serialize/pt file
+		serialized_file = self.manifest['model']['serializedFile']
+		model_pt_path = os.path.join(model_dir, serialized_file)
+		if not os.path.isfile(model_pt_path):
+			raise RuntimeError("Missing the model.pt file")
+
+		self.model = torch.jit.load(model_pt_path)
+
 		self.initialized = True
 
 	def preprocess(self, request) -> Tensor:
-		input_data: dict = json.load(request)
+		request_data: Any = request[0].get('data')
+		if request_data is None:
+			request_data = request[0].get('body')
+		input_data: dict = json.loads(request_data)
 		model_input: array = numpy.zeros(7, dtype=np.float16)
 		# Company Name Length
 		model_input[0] = int(input_data['name_len'])
@@ -36,7 +53,7 @@ class ModelHandler(BaseHandler):
 		# Industry
 		model_input[4] = int(input_data['industry'])
 		# Investor
-		model_input[3] = int(input_data['investor'])
+		model_input[5] = int(input_data['investor'])
 		# Last Valuation
 		half = sqrt(10)
 		scientific: list = '{:e}'.format(int(input_data['last_valuation'])).split('e')
@@ -46,18 +63,19 @@ class ModelHandler(BaseHandler):
 			power += 1
 		model_input[6] = power
 
-		return Tensor(model_input)
+		return Tensor(model_input.reshape(1, -1))
 
 	def inference(self, model_input, **kwargs) -> Tensor:
 		data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 		scaled_data: Tensor = Tensor(array(data_scaler.fit_transform(model_input)))
 
-		return self.model(scaled_data)
+		with torch.no_grad():
+			return self.model(scaled_data)
 
-	def postprocess(self, inference_output) -> str:
-		return '{:.2f}%'.format(inference_output * 100)
+	def postprocess(self, inference_output):
+		return array(inference_output.item())
 
-	def handle(self, data, context) -> str:
+	def handle(self, data, context):
 		model_input: Tensor = self.preprocess(data)
 		model_output = self.inference(model_input)
 
