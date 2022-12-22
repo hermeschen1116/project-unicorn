0ecebd43e800d766c41419b89bf595c6c37e10df
yangyx30678 <71892273+yangyx30678@users.noreply.github.com>
2022-12-22 18:48:09 +0800

    Updated front-end and a sample Json

--- src/La+ Program/app.py
+++ src/La+ Program/app.py
@@ -1,7 +1,9 @@
 # importing library
 import eel
 import numpy
+import os
 from math import sqrt
+import json
 # initialize
 
 def count_special_char(name: str) -> int:
@@ -13,12 +15,34 @@ def count_special_char(name: str) -> int:
 
 	return special_char_count
 
+path = os.path.dirname(os.path.realpath('__file__'))
+print(path)
 
-eel.init("./src/La+ Program")
 
+name_length = 0
+name_sp = ""
+country_value = 0
+
+
+def buildJSON(name, name_length, name_sp, nation, city, industry, investory, ls, fb) :
+    myDict = {
+    "name": name,
+    "name_len": name_length,
+    "name_sp": name_sp,
+    "country": country_value,
+    "city": city, 
+    "industry": industry,
+    "investory":investory,
+    "last_valuation":ls,
+    "feedback":fb
+    }
+    # 將 Python 資料轉為 JSON 格式，儲存至 output.json 檔案
+    with open("output.json", "w") as f:
+        json.dump(myDict, f, indent = 4)
+        
 @eel.expose
 # below should be our model
-def pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls):
+def pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls,fb):
     #frame = name,fy,city,nation,industry,founder,investory,tf,ct,os,ls
     #feature truly used:
     # name_len
@@ -28,87 +52,21 @@ def pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls):
     # industry
     # investor
     # last valuation
+    # Feedback
 
     # length of company name
     name_length = 0
     name_length = ("".join(name)).__len__()
     # special char counts of company name
     name_sp = count_special_char("".join(name))
-    #country name
-    country_n = ("".join(nation)).lower()
-    country_value = 0
-    if country_n == "united states":
-        country_value = 0
-    elif country_n == "china":
-        country_value = 1
 
-    #city
-    city_name = ("".join(city)).lower()
-    #default
-    city_value = 0
-    if city_name == "san francisco":
-        city_value = 0
-    elif city_name == "new york":
-        city_value = 1
-
-
-    #this sould be button or scroll to choose
-    industry_type = ("".join(industry)).lower()
-    #print(industry_type)
-    #default
-    industry_value = 0
-    #industry: dict = {'artificial intelligence': 0, 'fintech': 1, 'internet software & services': 2, 'analytics': 3, 'biotechnology': 4, 'health care': 5, 'e-commerce & direct-to-consumer': 6}
-    match industry_type:
-        case "artificial intelligence":
-            industry_value = 0
-        case "fintech":
-            industry_value = 1
-        case "internet software & services":
-            industry_value = 2
-        case "analytics":
-            industry_value = 3
-        case "biotechnology":
-            industry_value = 4
-        case "health care":
-            industry_value = 5
-        case "e-commerce & direct-to-consumer":
-            industry_value = 6
-    #test get value
-    #print(industry_value)
-
-    #this sould be button or scroll to choose
-    investor_type = ("".join(investory)).lower()
-    #default
-    investor_value = 0
-
-    #print(investor_type)
-
-    investory: dict = {'andreessen horowitz': 0, 'techstars': 1, 'alumni ventures': 2, 'y combinator': 3, 'sequoia capital': 4, '500 global': 5, 'insight partners': 6}
-    match investor_type:
-        case "andreessen horowitz":
-            investor_value = 0
-        case "techstars":
-            investor_value = 1
-        case "alumni ventures":
-            investor_value = 2
-        case "y combinator":
-            investor_value = 3
-        case "sequoia capital":
-            investor_value = 4
-        case "500 global":
-            investor_value = 5
-        case "insight partners":
-            investor_value = 6
-
-    #test get value
-    #print(investor_value)
-
-    #last valuation
-    last_valuation_value = 0
-    last_valuation_value = ("".join(ls)).__len__()-1
-
+    buildJSON(name, name_length, name_sp, nation, city, industry, investory, ls, fb)
+    
     output = "here should be prediction"
     return output
 
+eel.init(path)
 # start
 eel.start('myWebpage.html')
+
+
--- src/La+ Program/main.js
+++ src/La+ Program/main.js
@@ -1,5 +1,3 @@
-//這邊必須要async funciton 因為python返回需要時間，而JS 又不會block，
-//所以需要用async function 加上await去呼叫PY function
 async function fit(){
     var name = document.getElementById("name").value
     var fy = document.getElementById("Founded_Year").value
@@ -12,7 +10,8 @@ async function fit(){
     var ct = document.getElementById("Company_Type").value
     var os = document.getElementById("Operating_Status").value
     var ls = document.getElementById("Last_Valuation").value
-    var result = eel.pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls)
+    var fb = document.getElementById("trash").value
+    var result = eel.pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls,fb)
     result(call_Back) // a little bit weird https://youtu.be/IbGJaTkyuXA?t=239
 }   
 
