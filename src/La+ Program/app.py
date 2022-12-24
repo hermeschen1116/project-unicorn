8fab85731cdaa7e80cd06b01c5184387385c8163
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2022-12-24 23:49:50 +0800

    fix frontend

--- src/La+ Program/app.py
+++ src/La+ Program/app.py
@@ -1,75 +1,31 @@
-# changelog:
-# investory to investor in JSON
-# fix country not passing value
-# import pandas 
-
 # importing library
 import eel
 import numpy
-import os
-from math import sqrt
-import json
-import pandas as pd
+import torch
+
+
 # initialize
 
 def count_special_char(name: str) -> int:
-	special_char: list = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@']
+	special_char: list = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';',
+						  '<', '=', '>', '?', '@']
 	special_char_count: int = 0
 	for c in name:
 		if c in special_char:
 			special_char_count += 1
 
 	return special_char_count
-path = os.path.dirname(os.path.abspath(__file__))
 
-def buildJSON(name, name_length, name_sp, nation, city, industry, investor, ls, fb) :
-    myDict = {
-    "name": name,
-    "name_len": name_length,
-    "name_sp": name_sp,
-    "country": nation,
-    "city": city, 
-    "industry": industry,
-    "investor":investor,
-    "last_valuation":ls,
-    "feedback":fb
-    }
 
-    with open(path + "\output.json", "w") as f:
-        json.dump(myDict, f, indent = 4)
-        
 @eel.expose
-# below should be our model
-def pred(name,fy,city,nation,industry,founder,investor,tf,ct,os,ls,fb):
-    #frame = name,fy,city,nation,industry,founder,investory,tf,ct,os,ls
-    #feature truly used:
-    # name_len
-    # name special char
-    # city
-    # country
-    # industry
-    # investor
-    # last valuation
-    # Feedback
+def prediction(company: str, city: str, nation: str, industry: str, investor: str, last_valuation: str) -> str:
+	model = torch.jit.load('production.pt')
+	request: list = [len(company), count_special_char(company), nation, city, industry, investor, last_valuation]
+	model_input: torch.Tensor = torch.Tensor(numpy.array(request, dtype=int).reshape(1, -1))
 
-    # length of company name
-    name_length = 0
-    name_length = ("".join(name)).__len__()
-    # special char counts of company name
-    name_sp = count_special_char("".join(name))
+	return '{:.2f}%'.format(model(model_input).item() * 100)
 
-    buildJSON(name, name_length, name_sp, nation, city, industry, investor, ls, fb)
-    
-  
-    data = pd.read_json('output.json', typ='series')
-    from Inference import inference
-    output = inference(name_length, name_sp, nation, city, industry, investor, ls)
-    
-    # print ("{:.3%}".format(output))
-    return output
 
-eel.init(path)
+eel.init('.')
 # start
-eel.start('myWebpage.html')
-
-
+eel.start('laplusprogram.html', mode='default')
--- src/La+ Program/main.js
+++ src/La+ Program/main.js
@@ -1,22 +1,14 @@
-async function fit(){
-    var name = document.getElementById("name").value
-    var fy = document.getElementById("Founded_Year").value
-    var city = document.getElementById("City").value
-    var nation = document.getElementById("Nation").value
-    var industry = document.getElementById("Industry").value
-    var founder = document.getElementById("Founder").value
-    var investory = document.getElementById("Investor").value
-    var tf = document.getElementById("Total_Founding").value
-    var ct = document.getElementById("Company_Type").value
-    var os = document.getElementById("Operating_Status").value
-    var ls = document.getElementById("Last_Valuation").value
-    var fb = document.getElementById("trash").value
-    var result = eel.pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls,fb)
-    result(call_Back) // a little bit weird https://youtu.be/IbGJaTkyuXA?t=239
-}   
+async function request() {
+	let company_name = document.getElementById("Name").value
+	let city = document.getElementById("City").value
+	let nation = document.getElementById("Nation").value
+	let industry = document.getElementById("Industry").value
+	let investor = document.getElementById("Investor").value
+	let valuation = document.getElementById("Last_Valuation").value
+	await eel.prediction(company_name, city, nation, industry, investor, valuation)(call_Back)
+	// a little weird https://youtu.be/IbGJaTkyuXA?t=239
+}
 
-eel.expose(call_Back)
-
-function call_Back(output){
-    document.getElementById("res").textContent = output
+function call_Back(result) {
+	document.getElementById("prediction").textContent = result
 }
